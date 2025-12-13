52b3dd43728156c417655546142f8fef058378a4
mirsabayu-xelodigital <mirsabayu@xelodigital.com>
2025-12-13 12:05:52 +0700

    update : update gitignore and change on main.py

--- main.py
+++ main.py
@@ -7,29 +7,76 @@ import boostInterface as bi
 class main : 
     def __init__(self):
         self.value = "Hello, World!"
-        self.data = dp.DataProcesssing()
-        self.ada.initialize_model()
-        self.ada = ab.AdaBoost(self.data)
-        self.xgboost = xgb.XGBoost(self.data)
-        self.evaluator = ev.Evaluator()
+        # self.data = dp.DataProcesssing()
+        # self.ada.initialize_model()
+        # self.ada = ab.AdaBoost(self.data)
+        # self.xgboost = xgb.XGBoost(self.data)
+        # self.evaluator = None
 
     def run(self):
 
         boost = bi.BoostInterface()
 
-        boost = ab.Adaboost(self.data)
-        boost.initialize("X1")
-        self.value = boost.prediction_value()   
+        scenarios = [{"scenario" : "free",
+                      "model": "adaboost",
+                      "n_estimator": 50,
+                      "learning_rate": 0.1, 
+                      "max_depth": 3},
 
-        boost = xgb.XGBoost(self.data)
-        boost.initialize("X1")
-        self.value = boost.prediction_value()
+                      {"scenario" : "x1",
+                      "n_estimator": 50,
+                      "learning_rate": 0.1,
+                      "max_depth": 3},
+                      
+                      {"scenario" : "x2",
+                      "n_estimator": 50,
+                      "learning_rate": 0.1,
+                      "max_depth": 3},
+                      
+                      {"scenario" : "x1x2",
+                      "n_estimator": 50,
+                      "learning_rate": 0.1,
+                      "max_depth": 3},]
 
-        #rmse
-        self.evaluator = ev.Evaluator(boost.prediction_value(), self.data.get_y_test())
-        
-        
-        return self.value
+        print('hasil tanpa parameter tuning untuk adaboost dan xgboost')
+        for scenario in scenarios:
+            boost = ab.Adaboost(self.data)
+            boost.initialize(scenario)
+            print(f"ada boost successfully trained for scenario {scenario}")
+            self.evaluator = ev.Evaluator(boost.prediction_value(), self.data.get_y_test())
+            # RMSE
+            print(f"nilai rmse untuk ada boost : {self.evaluator.get_rmse()}")
+            # MSE        
+            print(f"nilai mse untuk ada boost : {self.evaluator.get_mse()}")
+            # MAE
+            print(f"nilai mae untuk ada boost : {self.evaluator.get_mae()}")
+            # MAPE
+            print(f"nilai mape untuk ada boost : {self.evaluator.get_mape()}")
+            # R2 Score
+            print(f"nilai r2 score untuk ada boost : {self.evaluator.get_r2_score()}")
+            print("--------------------------------------------------")
+            print("--------------------------------------------------")
+            boost = xgb.XGBoost(self.data)
+            boost.initialize(scenario)
+            print(f"xg boost successfully trained for scenario {scenario}")
+            self.evaluator = ev.Evaluator(boost.prediction_value(), self.data.get_y_test())
+            # RMSE
+            print(f"nilai rmse untuk xg boost : {self.evaluator.get_rmse()}")
+            # MSE        
+            print(f"nilai mse untuk xg boost : {self.evaluator.get_mse()}")
+            # MAE
+            print(f"nilai mae untuk xg boost : {self.evaluator.get_mae()}")
+            # MAPE
+            print(f"nilai mape untuk xg boost : {self.evaluator.get_mape()}")
+            # R2 Score
+            print(f"nilai r2 score untuk xg boost : {self.evaluator.get_r2_score()}")
+            print("--------------------------------------------------")
+            print("--------------------------------------------------")
+            print("\n\n")
+        pass
+        print("hasil menggunakan parameter tuning menggunakan gridCV")
+
+
     
 
 if __name__ == "__main__":
