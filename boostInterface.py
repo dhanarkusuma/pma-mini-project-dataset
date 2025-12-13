efad2466728ff16ccac18a3bf113206682bbe1ae
mirsabayu-xelodigital <mirsabayu@xelodigital.com>
2025-12-13 12:26:48 +0700

    update interface boosInterface to integrate parameter_tuning

--- boostInterface.py
+++ boostInterface.py
@@ -9,3 +9,7 @@ class BoostInterface:
     @abstractmethod
     def prediction_value(self):
         pass
+
+    @abstractmethod
+    def parameter_tuning(self, param_grid, scoring, cv):
+        pass
